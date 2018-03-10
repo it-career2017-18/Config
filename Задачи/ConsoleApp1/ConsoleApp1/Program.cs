025b79698b00da5e98fa87dd26ee12266afcce44
Samuil <35292702+sami119@users.noreply.github.com>
2018-03-10 14:33:18 +0200

    ZADACHAA 1

---
+++ Задачи/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int secKey = int.Parse(Console.ReadLine());
+            string[] webData = new string[3];
+            var siteLoss = 0.0;
+            string[] names = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                webData = Console.ReadLine().Split(' ').ToArray();
+                siteLoss += double.Parse(webData[1]) * double.Parse(webData[2]);
+                names[i] = webData[0];
+            }
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Total Loss: {0:F20}", siteLoss);
+            Console.WriteLine("Security Token: {0}", Math.Pow(secKey, N));
+            Console.ReadKey();
+        }
+    }
+}
