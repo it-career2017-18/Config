3a204dc7e152a146363f9305abfd2216d4eb4f7b
Samuil <35292702+sami119@users.noreply.github.com>
2018-03-10 16:21:11 +0200

    sami119

---
+++ Задачи/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = Console.ReadLine().Split(' ').ToArray();
+            string[] mergeDivide = Console.ReadLine().Split(' ').ToArray();
+            while(mergeDivide[0] !=  "3:1")
+            {
+                if (mergeDivide[0] == "merge")
+                {
+                    data = 
+                }
+                if (mergeDivide[0] == "divide")
+                {
+
+                }
+            }
+        }
+    }
+}
