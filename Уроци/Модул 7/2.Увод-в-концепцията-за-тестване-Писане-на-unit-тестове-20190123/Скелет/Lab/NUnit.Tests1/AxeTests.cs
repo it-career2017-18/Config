7bb379b30d5c7e4e47e1d13aa92fdf283bbdb4d3
Samuil <35292702+sami119@users.noreply.github.com>
2019-02-10 01:51:09 +0200

    unit test - solution

---
+++ Уроци/Модул 7/2.Увод-в-концепцията-за-тестване-Писане-на-unit-тестове-20190123/Скелет/Lab/NUnit.Tests1/AxeTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            // TODO: Add your test code here
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            axe.Attack(dummy);
+
+            Assert.AreEqual(1, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        }
+
+        [Test]
+        public void BrokenAxeCanAttack()
+        {
+            Axe axe = new Axe(2, 2);
+            Dummy dummy = new Dummy(20, 20);
+
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
